5c6dec50ecc9a2dd146824e2202a7d12bab3c806
bhavanakmys <myskbhavana@gmail.com>
2020-07-03 12:45:22 +0530

    Updates

---
+++ precache-manifest.8822f364c0761dfcfe825b297500c91e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8f808276ad850126f3666896a0a57f",
+    "url": "/KavachNetworks-wedlockZ_FE/index.html"
+  },
+  {
+    "revision": "3c7bc2260a5b0b3049c1",
+    "url": "/KavachNetworks-wedlockZ_FE/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3c69a63592a01cf2df65",
+    "url": "/KavachNetworks-wedlockZ_FE/static/css/main.11fc5ca3.chunk.css"
+  },
+  {
+    "revision": "3c7bc2260a5b0b3049c1",
+    "url": "/KavachNetworks-wedlockZ_FE/static/js/2.8a32d6a0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/KavachNetworks-wedlockZ_FE/static/js/2.8a32d6a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c69a63592a01cf2df65",
+    "url": "/KavachNetworks-wedlockZ_FE/static/js/main.abc8603a.chunk.js"
+  },
+  {
+    "revision": "7548c1ccb481352f6c2c",
+    "url": "/KavachNetworks-wedlockZ_FE/static/js/runtime-main.b03098ae.js"
+  }
+]);
