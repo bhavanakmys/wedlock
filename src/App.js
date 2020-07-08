cc055a096b2f3313f099a720a913222e7d50d66c
Himanshu <himanshuV110@gmail.com>
2020-07-08 16:05:58 +0530

    css added registartion form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React  from 'react';
 
-import './App.css';
-import Header from './components/Header/Header';
-import Head from './components/Head/Head';
-import { Headb } from './components/Headb/Headb';
 
-import Middle from './components/midddle/Middle';
-import Footer from './components/Footer/Footer';
-import Mid from './components/Mid/Mid';
-import Login from './components/Login/Login';
-import login from './components/Headb/login';
+import './App.css';
 import RegistrationC from './components/RegstrationC/RegistratioC';
+import Headb from './components/Headb/Headb';
 
 function App() {
   return (
     <div>
- {/* <Headb/>
-  <Middle/>
- <Footer/>
-  <Login/>*/}
-
+ 
+   
   <RegistrationC/>
    
     </div>
